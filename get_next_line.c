254810728d5cf881e09c7d3a94b5dbc5e5763aa5
Mariah Simmons <msimmons@ox-t3.21-school.ru>
2019-10-05 14:21:02 +0300

    add func FromReadToList, modified func checkfd

--- get_next_line.c
+++ get_next_line.c
@@ -1,14 +1,19 @@
-#include "libft.h"
+##include "libft.h"
 #include "get_next_line.h"
 
-int			checkfd(int fd)
+t_list		*checkfd(int fd, t_list *fd_list[])
 {
+	t_list	*list;
+	char buf[BUFF_SIZE + 1];
+
 	if (fd < 0)
 		return (NULL); // -1 ??
-    return (0);
+	if (fd_list[fd] != NULL)
+		list = fd_list[fd];
+	return (list);
 }
-
-void     del(void *ptr, size_t s)
+/*
+void     	del(void *ptr, size_t s)
 {
 	free(ptr);
 	(void) s;
@@ -24,38 +29,57 @@ char		*get_text_from_list_to_line(t_list *src, char *dst)
 
 	ft_lstdelone(&src, del);
 }
+*/
+int			fromReadToList(t_list *list, const int fd)
+{
+	int ret;
+	int flag;
+	char buf[BUFF_SIZE + 1];
+
+	flag = 1;
+	while ((ret = read(fd, buf, BUFF_SIZE))) // считали из файла в массив buf кол-во бафсайз (или меньше, если строка закончилась)
+	{
+
+		if (ret == -1)
+			return (ret);
+		else if (flag == 1)
+		{
+			printf("ret == %d\n", ret);
+			list = ft_lstnew(buf, BUFF_SIZE + 1);
+			flag = 0;
+		}
+		else if (ret != 1)                                // если остался только \n для него уже не создаем лист
+			ft_lstadd(&list, ft_lstnew(buf, BUFF_SIZE + 1)); // скопировали в узел списка из буфера часть строки
+	}
+	if (flag == 1) {
+		printf("flag =1 wtf");
+		return (0);
+	}
+	else return (1);
+}
 
 int			get_next_line(const int fd, char **line)
 {
-    int		cfd;
-    int 	ret;
-    char 	buf[BUFF_SIZE + 1];
-    t_list	*list;
+	t_list	*list;
+	t_list	*fd_list[OPEN_MAX];
+	int ret;
 
+	list = checkfd(fd, fd_list);
+	ret = fromReadToList(list, fd);
 
-    cfd = checkfd(fd);
-    //if file - >   OPEN IT
-    //if stdout - > fd == 1
-    //if stdin - >  fd == 0
-    while (ret = read(cfd, buf, BUFF_SIZE)) // считали из файла в массив buf кол-во бафсайз (или меньше, если строка закончилась)
-	{
-    	if (ret == -1)
-			return (ret);
-    	else if (!list)
-    		list = ft_lstnew(buf, BUFF_SIZE + 1);
-		else
-			list = ft_lstadd(&list, ft_lstnew(buf, BUFF_SIZE + 1); // скопировали в узел списка из буфера часть строки
-	}
-    if (list)
-    {
-		*line = get_text_from_list_to_line(list, *line);
-		ret = 1;
-	}
-    else
-    	ret = 0;
-	return (ret); // прочитали пустой файл
+	// if (list)
+	// {
+	//	*line = get_text_from_list_to_line(list, *line);
+	//	ret = 1;
+	//}
+	return (ret); // ret = 1, 0 , -1
 }
 
+int			main()
+{
+	char *ptr = (char *)malloc(sizeof(*ptr) * 500);
+	get_next_line(open("a", O_RDONLY), &ptr);
+}
 /*
  * The return value can be 1, 0 or -1 depending on whether a line has been read,
 when the reading has been completed, or if an error has happened respectively.
