4dd500a9cc2c78d3f2761467bdbfe09a22cc1c0f
Mariah Simmons <msimmons@vo-d4.21-school.ru>
2019-10-06 18:38:14 +0300

    check_fd removed completely, trying to get it work with BUF_SIZE 1 and >7

--- get_next_line.c
+++ get_next_line.c
@@ -1,16 +1,8 @@
 ##include <stdio.h>
+#include <stdio.h>
 #include "libft.h"
 #include "get_next_line.h"
 static t_list *fd_list[OPEN_MAX];
-
-int		checkfd(int fd)
-{
-    char buf[BUFF_SIZE];
-
-    if (fd < 0)
-        return (-1);
-    return (1);
-}
 
 void     	del(void *ptr, size_t s)
 {
@@ -34,12 +26,12 @@ int		check_buf(char *buf, const int fd)
 {
     size_t i;
     int lineRead;
-    char *before_n = (char *)malloc(sizeof(char) * ft_strlen(buf) + 1);
+    char *before_n;
 
-    before_n = ft_strcpy(before_n, buf);
+    before_n = buf;          // отсюда будем брать для списка часть строки до \n
     lineRead = 0;
     i = 0;
-    while (*buf != '\n' && *buf != '\0' && *buf != EOF && i < BUFF_SIZE)
+    while (*buf != '\n' && *buf != '\0' && i < BUFF_SIZE)
     {
         printf("%zu-ый символ буфера = %c\n", i, *buf);
         buf++;
@@ -51,21 +43,22 @@ int		check_buf(char *buf, const int fd)
         printf("нашел символ новой строки! теперь там ноль\n");
         *buf = '\0';
         buf++;
-        printf("перешел на след символ и это = %c\n", *buf);
-        if (i > 0)
+        printf("перешел на след символ и это: %c\n", *buf);
+        printf("buf = %s\n", buf);
+        if (i > 0)                          // если i > 0, то в буфере перед \n что-то было, кладем это в список
         {
             if (fd_list[fd] == NULL)
                 fd_list[fd] = ft_lstnew(before_n, ft_strlen(before_n));
             else // возможно, этот else не нужен
-            {
                 ft_lstadd(&(fd_list[fd]), ft_lstnew(before_n, ft_strlen(before_n)));
-                printf("в check_ buf: создан узел списка с адресом: %p\n", fd_list[fd]);
-                printf("в check_ buf: в нем: %s\n", fd_list[fd]->content);
-                printf("в check_ buf: указывает на: %p\n", fd_list[fd]->next);
-                printf("смотрю в fd_list, там %p\n", fd_list[fd]);
-            }
+            printf("в check_ buf: создан узел списка с адресом: %p\n", fd_list[fd]);
+            printf("в check_ buf: в нем: %s\n", fd_list[fd]->content);
+            printf("в check_ buf: указывает на: %p\n", fd_list[fd]->next);
+            printf("смотрю в fd_list, там %p\n", fd_list[fd]);
+
+            //   free(before_n);
         }
-        if (ft_isprint(*buf))
+        if (ft_isprint(*buf))              // если в буфере после \n что-то осталось, кладем остаток в список
         {
             if (fd_list[fd] == NULL)
                 fd_list[fd] = ft_lstnew(buf, ft_strlen(buf));
@@ -90,7 +83,7 @@ int			fromReadToList(const int fd)
     {
         if (ret == -1)
             return (ret);
-        printf("в буфере = %s \n", buf);
+        printf("в буфере: %s\n", buf);
         printf("проверяю буфер... \n");
         lineR = check_buf(buf, fd);                               // проверка на наличие \n в буфере
         if (fd_list[fd] == NULL)
@@ -105,29 +98,27 @@ int			fromReadToList(const int fd)
             break;
     }
 
-    //нужна проверка считана ли линия или закончено считывание
-    return (1);
+    if (ret == 0)
+        return (0);
+    else
+        return (1);
 }
 
 int			get_next_line(const int fd, char **line)
 {
     int ret;
-    int cfd;
 
-    cfd = checkfd(fd);
-    if (cfd < 0)
+    if (fd < 0)
         return (-1);
     ret = fromReadToList(fd);
     //if file - >   OPEN IT
     //if stdout - > fd == 1
     //if stdin - >  fd == 0
 
-    // if (list)
-    // {
-    //	*line = get_text_from_list_to_line(list, *line);
-    //	ret = 1;
-    //}
-    return (ret); // ret = 1, 0 , -1
+    //  *line = get_text_from_list_to_line(list, *line);
+    //
+    // add func for cleaning the list;
+    return (ret);
 }
 
 int main()
@@ -154,6 +145,9 @@ int main()
     //<===== CHECK =====>
 }
 /*
+ * На повестке дня:
+ * 1) Чтобы работало с буфером размером 1, >7
+ * С буфером больше 7, приставляет рандомный символ
  * The return value can be 1, 0 or -1 depending on whether a line has been read,
 when the reading has been completed, or if an error has happened respectively.
  *
