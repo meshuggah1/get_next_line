923f42b1d55c223220c557a2faead17b6a92165f
Mariah Simmons <msimmons@ga-l5.21-school.ru>
2019-10-20 18:07:06 +0300

    main removed from gnl to another file; some fixes

--- get_next_line.c
+++ get_next_line.c
@@ -6,22 +6,30 @@ int        str_and_fd(char *ptr, char **line, int fd)
 {
     size_t i;
     char *strcpy;
-
-    strcpy = (char *)malloc(sizeof(char) * ft_strlen(ptr));
-    strcpy = ft_strcpy(strcpy, ptr);
+    //ptr = сохранненая в fd_list часть строки или буфер с новой строкой
+    //strcpy = (char *)malloc(sizeof(char) * ft_strlen(ptr));
+    strcpy = ft_strdup(ptr);
     i = 0;
-    //printf("str_and_fd: strcpy = %s, line = %s\n", strcpy, *line);
+    printf("str_and_fd: strcpy = %s, strcpy address = %p\nline = %s, line address = %p */\n", strcpy, strcpy, *line, *line);
     while (i < BUFF_SIZE && strcpy[i] != '\n')
         i++;
     if (!(**line))
-        *line = ft_strsub(strcpy, 0, i ); // кладем все до новой строки включительно
-    else
-        *line = ft_strjoin(*line, ft_strsub(strcpy, 0, i));
-    if (strcpy[i] == '\n')
     {
-        fd_list[fd] = &strcpy[i + 1]; // оставляем в fd_list хранить все, что после новой строки
+        *line = ft_strsub(strcpy, 0, i); // кладем все до новой строки включительно  ЗДЕСЬ МЕНЯЕТСЯ АДРЕС ЛАЙН
+        printf("line created: %s\n", *line);
+    }
+    else {                                      // дополняем если в буфере поместилось меньше, чем строка до \n
+        *line = ft_strjoin(*line, ft_strsub(strcpy, 0, i));
+        printf("line joined: %s\n", *line);
+    }
+    if (strcpy[i] == '\n' && strcpy[i + 1] != '\0')
+    {
+        free(fd_list[fd]);
+        fd_list[fd] = ft_strdup(&(strcpy[i + 1])); // оставляем в fd_list хранить все, что после новой строки
+        free(strcpy);
         return (1);
     }
+    free(strcpy);
     return (0);
 }
 
@@ -35,14 +43,14 @@ int			get_next_line(const int fd, char **line)
         return (-1);
     if (fd_list[fd]) // если огрызок есть, сначала читаем его
     {
-        //printf("огрызок на месте! = %s по адресу %p\n", fd_list[fd], &fd_list[fd]);
+        printf("substring detected! = %s, it's address = %p, line = %s, line address = %p\n", fd_list[fd], &fd_list[fd], *line, *line);
         ret = str_and_fd(fd_list[fd], line, fd); // если вернули 1, то считали строку
         if (ret)                    // в огрызке была новая строка - считали, можно выходить
             return (1);
+        // далее идем если (1)огрызка не было или (2)в нем не было новой строки
     }
-    // далее идем если (1)огрызка не было или (2)в нем не было новой строки
-
-    while ((ret = read(fd, buf, BUFF_SIZE))) // считали из файла в массив buf кол-во бафсайз (или меньше, если строка закончилась)
+    while ((ret = read(fd, buf,
+                       BUFF_SIZE))) // считали из файла в массив buf кол-во бафсайз (или меньше, если строка закончилась)
     {
         if (ret == -1)
             return (ret);
@@ -56,29 +64,7 @@ int			get_next_line(const int fd, char **line)
     }
     return (ret);
 }
-/*
-int main()
-{
-    int fd = open("a", O_RDONLY);
-    int fd1 = open("b", O_RDONLY);
 
-    char *ptr = (char *)malloc(sizeof(*ptr) * 500);
-    printf("ВЫЗОВ GNL ДЛЯ a...\n");
-    get_next_line(fd, &ptr);
-    printf("ptr = %s\n", ptr);
-    printf("vyzov GNL dlya b...\n");
-    get_next_line(fd1, &ptr);
-    printf("ВЫЗОВ GNL ДЛЯ a... 2й раз\n");
-    get_next_line(fd, &ptr);
-    printf("vyzov GNL dlya b...\n");
-    get_next_line(fd1, &ptr);
-    printf("ptr = %s\n", ptr);
-
-    printf("ВЫЗОВ GNL ДЛЯ a... 3й раз\n");
-    get_next_line(fd, &ptr);
-    printf("ptr = %s\n", ptr);
-
-}*/
 /*
  * The return value can be 1, 0 or -1 depending on whether a line has been read,
 when the reading has been completed, or if an error has happened respectively.
